8293a51fa94ace6cec6194d204d1e28140cc440d
Swopper050 <robocop@brams-mbp.home>
2020-06-05 10:08:41 +0200

    added simulation plots

--- Agents.py
+++ Agents.py
@@ -3,7 +3,9 @@
 
 import numpy as np
 from random import seed
-from random import randint
+from random import randint, uniform, choice
+from itertools import zip_longest
+import names
 
 seed(1)
 
@@ -17,9 +19,13 @@ class Agent:
         self.last_deal_price = -1 ## The information about the last transaction the agent made.
         self.min_price = config.lowprice
         self.max_price = config.highprice
+        self.price_interval = 0.50
+        self.name = names.get_first_name()
 
     ## Adding another price to the possible prices.
     def add_possible_price(self, price):
+        if price < self.min_price or price > self.max_price:
+            return
         self.possible_prices = np.append(self.possible_prices, price)
         self.possible_prices = np.unique(self.possible_prices)
         self.possible_prices = np.sort(self.possible_prices)
@@ -45,26 +51,34 @@ class Agent:
 class Buyer(Agent):
     def __init__(self, vector_prices, config):
         Agent.__init__(self, vector_prices, config)
-        self.sharing_knowledge = [1,-1]
+        self.type = 'buyer'
+        self.sharing_knowledge = [1, -1]
 
     ## As a Buyer, if someone made a deal for less price than some of your possible scenarios, you will
     ## remove these ones, to achieve deals for less or equal value than that one (good ones for you).
-    def adquire_knowledge(self, new_knowledge):
-        for number in range(new_knowledge[1], self.max_price+1):
+    def acquire_knowledge(self, new_knowledge):
+        if new_knowledge[1] == -1:
+            return
+
+        for number in np.arange(new_knowledge[1] + self.price_interval, self.max_price, self.price_interval):
             self.remove_possible_price(number)
 
     def update_share_information(self):
-        value = randint(self.last_deal_price, self.max_price+1)
-        self.sharing_knowledge = [1, value]
+        if self.last_deal_price == -1:
+            self.sharing_knowledge = [1, -1]
+        else:
+            value = round(uniform(self.last_deal_price, self.max_price) * 2) / 2.
+            self.sharing_knowledge = [1, value]
 
     def pick_seller(self, available_sellers):
         """
         A Buyer should select a seller from all available sellers. Given is a list of
-        available sellers. Currently simply the first seller is selected, although a
+        available sellers. Currently simply a random seller is selected, although a
         more sophisticated method might be implemented for this
         """
-
-        return available_sellers[0]
+        if available_sellers:
+            return choice(available_sellers)
+        return None
 
     def negotiation_with_seller(self, seller):
         """
@@ -76,33 +90,61 @@ class Buyer(Agent):
         price when it is not considered possible in their worlds.
         """
 
-        seller_price_generator = seller.get_price_generator()
-        import pdb; pdb.set_trace()
-        return x
+        union_prices = set(seller.possible_prices) & set(self.possible_prices)
+
+        # If empty, there will be no transaction
+        if not union_prices:
+            self.last_deal_price = seller.last_deal_price = -1
+        else:
+            union_prices = list(union_prices)
+            middle_price = union_prices[len(union_prices) // 2]
+            seller.last_deal_price = self.last_deal_price = middle_price
+
+        return self.last_deal_price
+
+    def update_prices(self):
+        """ Updates prices if no transaction was made. """
+        if self.last_deal_price == -1:
+            # Add a higher price to the possible prices
+            if len(self.possible_prices) != 0:
+                self.add_possible_price(self.possible_prices[-1] + self.price_interval)
+            else:
+                self.add_possible_price(self.min_price)
 
 
 class Seller(Agent):
     def __init__(self, vector_prices, config):
         Agent.__init__(self, vector_prices, config)
+        self.type = 'seller'
         self.sharing_knowledge = [2, -1]
 
-    ## As a seller, if someone made a deal for more price than some of your possible scenarios, you will
-    ## remove these ones, to achieve deals for more or equal value than that one (good ones for you).
-    def adquire_knowledge(self, new_knowledge):
-        for number in range(self.min_price, new_knowledge[1]):
-            self.remove_possible_price(number)
+    def acquire_knowledge(self, new_knowledge):
+        """As a seller, if someone made a deal for more price than some of your possible scenarios, you will
+        remove these ones, to achieve deals for more or equal value than that one (good ones for you)."""
+        if new_knowledge[1] == -1:
+            return
+
+        for number in np.arange(self.min_price, new_knowledge[1] + self.price_interval, self.price_interval):
+            if len(self.possible_prices) > 1:  # We do not want to delete the last price
+                self.remove_possible_price(number)
 
     def update_share_information(self):
-        ## based on the number, "randomize" -> second value of knowledge
-        value = randint(self.min_price, self.last_deal_price+1)
-        self.sharing_knowledge = [2, value]
-
-    def get_price_generator(self):
         """
-        Yields prices during negotiation according to some negotiation scheme. These can
-        be varied, but for now the most simple negotatiation scheme is used:
-            - a decreasing price
+        Updates information this agent have about the last deal. If no deal was made,
+        the agent will share no information (setting it to -1)
         """
+        if self.last_deal_price == -1:
+            self.sharing_knowledge = [1, -1]
+        else:
+            ## based on the number, "randomize" -> second value of knowledge
+            value = round(uniform(self.min_price, self.last_deal_price) * 2) / 2.
+            self.sharing_knowledge = [2, value]
 
-        for price in reversed(self.possible_prices):
-            yield price
+    def update_prices(self):
+        """ Updates prices if no transaction was made. """
+        if self.last_deal_price == -1:
+            # Add a lower price to the possible worlds
+            if len(self.possible_prices) != 0:
+                self.add_possible_price(self.possible_prices[0] - self.price_interval)
+            else:
+                self.add_possible_price(self.max_price)
--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@ import argparse
 import copy
 from itertools import chain
 import numpy as np
+import matplotlib.pyplot as plt
 import random
 
 from Agents import Buyer, Seller
@@ -12,22 +13,58 @@ def simulation(config):
 
     buyers, sellers = initialize_agents(config)
 
+    av_buyer_prices = []
+    av_seller_prices = []
+    av_transaction_prices = []
+
     for day in range(config.ndays):
+
         # Make all trades during day
         random.shuffle(buyers)  # Let buyers pick sellers in random order
         available_sellers = copy.copy(sellers)
+        day_transaction_prices = []
         for buyer in buyers:
+            # Let a buyer pick a seller, if no more sellers are available, None is returned
             seller = buyer.pick_seller(available_sellers)
-            buyer.negotiation_with_seller(seller)
-            available_sellers.remove(seller)
+            if seller is not None:
+                # Negotiate with the selected seller and remember the transaction price
+                transaction_price = buyer.negotiation_with_seller(seller)
+                if transaction_price != -1:  # There was a deal
+                    day_transaction_prices.append(transaction_price)
+                    available_sellers.remove(seller)
 
-        agent_interactions(buyers, n_interactions=2)
-        agent_interactions(sellers, n_interactions=2)
+        # Update prices --> If an agent had no transaction, add more prices
+        # Setup knowledge for sharing knowledge
         for agent in chain(buyers, sellers):
-            agent.update_worlds()
+            agent.update_prices()
+            agent.update_share_information()
+
+        # Let buyers interact with buyers and sellers with sellers (pairs are selected randomly)
+        agent_interactions(buyers, n_interactions=20)
+        agent_interactions(sellers, n_interactions=20)
+
+        # Store averages during the day for plotting
+        av_transaction_prices.append(np.mean(day_transaction_prices))
+        av_seller_prices.append(np.mean([min(seller.possible_prices) for seller in sellers]))
+        av_buyer_prices.append(np.mean([max(buyer.possible_prices) for buyer in buyers]))
+
+    # Plot the simulation
+    plt.plot(av_seller_prices, label="Average seller price")
+    plt.plot(av_buyer_prices, label="Average buyer price")
+    plt.plot(av_transaction_prices, label="Average transaction price")
+    plt.xlabel("Days")
+    plt.ylabel("Price")
+    plt.legend()
+    plt.show()
+
 
 
 def initialize_agents(config):
+    """
+    Initializes agents using the config files. Does 2 things:
+    Makes a list of buyers, all buyers have a random price range from (lowest - random number)
+    Makes a list of sellers, all sellers have a random price range from (random number - highest)
+    """
 
     buyer_prices = [np.arange(config.lowprice, get_random_high_buyer(config.lowprice, config.highprice), .50)
                     for _ in range(config.nbuyers)]
@@ -39,11 +76,13 @@ def initialize_agents(config):
 
 
 def get_random_high_buyer(lowprice, highprice):
+    """ Generates a random price (upper range) for a buyer. Used when initializing prices """
     diff = highprice - lowprice
     return round(random.uniform(lowprice, lowprice + .75 * diff) * 2) / 2.
 
 
 def get_random_low_seller(lowprice, highprice):
+    """ Generates a random price (lower range) for a seller. Used when initializing prices """
     diff = highprice - lowprice
     return round(random.uniform(lowprice + .25 * diff, highprice) * 2) / 2.
 
@@ -55,12 +94,18 @@ def agent_interactions(agents, n_interactions=10):
     """
 
     n_agents = len(agents)
+    if n_agents < 2:
+        return
     for _ in range(n_interactions):
         # Select two random agents
         agent1_idx, agent2_idx = random.sample(range(n_agents), 2)
         agent1 = agents[agent1_idx]
         agent2 = agents[agent2_idx]
-        agent1.knowledge_exchange(agent2)
+        #print(f"{agent1.type} {agent1.name} exchanges knowledge with {agent2.type} {agent2.name}")
+        #print(f"{agent1.name} shares {agent1.sharing_knowledge}")
+        #print(f"{agent2.name} shares {agent2.sharing_knowledge}")
+        agent1.acquire_knowledge(agent2.sharing_knowledge)
+        agent2.acquire_knowledge(agent1.sharing_knowledge)
 
 
 if __name__=="__main__":
@@ -93,4 +138,5 @@ if __name__=="__main__":
                         default=10.0,
                         help='Highest price possible in the complete simulation')
     config = parser.parse_args()
+    #print("\n\n\n\n\n")
     simulation(config)
